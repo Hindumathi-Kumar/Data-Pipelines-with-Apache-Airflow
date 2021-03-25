739b6b87a7fd751f793aeecc8d03a1153386186a
Hindumathi-Kumar <80439546+Hindumathi-Kumar@users.noreply.github.com>
2021-03-25 13:30:05 +1100

    Add files via upload

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -3,33 +3,42 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-
-class HasRowsOperator(BaseOperator):
+class DataQualityOperator(BaseOperator):
     """
-    Data quality check to ensure that the specified table has rows.
+    Performs data quality checks by running sql statements to validate the data.
 
     :param redshift_conn_id: reference to a specific redshift cluster hook
     :type redshift_conn_id: str
     :param table: destination fact table on redshift.
     :type table: str
+    :param query: sql statement to validate the data.
+    :type query: str
     """
+
+    ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
+                 # Define your operators params (with defaults) here
                  redshift_conn_id="",
                  table="",
+                 query="",
                  *args, **kwargs):
 
-        super(HasRowsOperator, self).__init__(*args, **kwargs)
-        self.table = table
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        # Map params here
         self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.query = query
 
     def execute(self, context):
+        self.log.info(f'Checking DataQuality for {self.table}')
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
+        self.log.info(f'Running query: {self.query}')
+        records = redshift_hook.get_records(self.query)
         if len(records) < 1 or len(records[0]) < 1:
             raise ValueError(f"Data quality check failed. {self.table} returned no results")
         num_records = records[0][0]
-        if num_records < 1:
+        if num_records != 0:
             raise ValueError(f"Data quality check failed. {self.table} contained 0 rows")
-        logging.info(f"Data quality on table {self.table} check passed with {records[0][0]} records")
+        logging.info(f'PASSED DATA QUALITY CHECK!! for {self.table}')
